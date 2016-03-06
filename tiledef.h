dcaa0e73eeaacce9ddbce6497cc65b5055768406
Takao Kido <gigan820@gmail.com>
2016-03-07 08:56:15 +0900

    change constants from define to const variable

--- tiledef.h
+++ tiledef.h
@@ -1,35 +1,37 @@
 #ifndef TILEDEF_INCLUDE
 #define TILEDEF_INCLUDE
 
+#include "typedef.h"
+
 //color flag definitions
-#define RED 0x0f	//00001111
-#define BLACK RED	//00001111
-#define WHITE 0x00	//00000000
+const TILE RED=0x0f;	//00001111
+const TILE BLACK=RED;	//00001111
+const TILE WHITE=0x00;	//00000000
 
 //tile data definitions
-#define NULLNODE 0	//00000000
-#define RPLUS 0x0c	//00001100
-#define WPLUS 0x03	//00000011
-#define RSLSH 0x0a	//00001010
-#define WSLSH 0x05	//00000101
-#define RBSLS 0x09	//00001001
-#define WBSLS 0x06	//00000110
+const TILE NULLNODE=0;	//00000000
+const TILE RPLUS=0x0c;	//00001100
+const TILE WPLUS=0x03;	//00000011
+const TILE RSLSH=0x0a;	//00001010
+const TILE WSLSH=0x05;	//00000101
+const TILE RBSLS=0x09;	//00001001
+const TILE WBSLS=0x06;	//00000110
 
 //position bit definitions
-#define UPOS 0x01	//00000001
-#define DPOS 0x02	//00000010
-#define LPOS 0x04	//00000100
-#define RPOS 0x08	//00001000
+const TILE UPOS=0x01;	//00000001
+const TILE DPOS=0x02;	//00000010
+const TILE LPOS=0x04;	//00000100
+const TILE RPOS=0x08;	//00001000
 
 //character definitions
-#define C_PLUS '+'
-#define C_SLSH '/'
-#define C_BSLS '\\'
+const char C_PLUS='+';
+const char C_SLSH='/';
+const char C_BSLS='\\';
 
 //shift number definitions
-#define U_SHIFT 0
-#define D_SHIFT 1
-#define L_SHIFT 2
-#define R_SHIFT 3
+const int U_SHIFT=0;
+const int D_SHIFT=1;
+const int L_SHIFT=2;
+const int R_SHIFT=3;
 
 #endif
